c1cdd2d8197d9d462a8d447d47f396023542f3e6
Marcelo <marcelo.agudelo@gmail.com>
2020-02-05 13:07:32 -0500

    Initial Commit

---
+++ BusinessInventory-swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BusinessInventory-swift
+//
+//  Created by Marcelo Agudelo on 2020-02-05.
+//  Copyright © 2020 Marcelo Agudelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
